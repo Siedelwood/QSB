517bd5db7bb1b7a239c54c89aa582ec9c1edc9a9
totalwarANGEL <n3crosh8d3@gmail.com>
2023-02-11 14:25:57 +0100

    FIX: Add missing script command

--- qsb/lua/modules/QSB_1_Requester/source.lua
+++ qsb/lua/modules/QSB_1_Requester/source.lua
@@ -52,6 +52,12 @@ ModuleRequester = {
 -- Global
 
 function ModuleRequester.Global:OnGameStart()
+    API.RegisterScriptCommand(
+        "Cmd_SetLanguageResult",
+        function(_PlayerID, _Language)
+            Swift.Text:OnLanguageChanged(_PlayerID, nil, _Language);
+        end
+    );
 end
 
 function ModuleRequester.Global:OnEvent(_ID, ...)
