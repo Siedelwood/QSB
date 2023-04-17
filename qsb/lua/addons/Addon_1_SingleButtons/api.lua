e9a64c53716b44b6f734a620611dfa86d8d0931f
Jean Baumgarten <jelumar@jlmb.eu>
2023-04-17 10:01:41 +0200

    implementation for single stop

--- qsb/lua/addons/Addon_1_SingleButtons/api.lua
+++ qsb/lua/addons/Addon_1_SingleButtons/api.lua
@@ -5,6 +5,7 @@
 -- <b>Vorausgesetzte Module:</b>
 -- <ul>
 -- <li><a href="qsb.html">(0) Basismodul</a></li>
+-- <li><a href="modules.qsb_2_buildingui.qsb_2_buildingui.html">(2) Gebäudeschalter</a></li>
 -- </ul>
 --
 -- @within Modulbeschreibung
@@ -14,20 +15,70 @@
 API = API or {}
 
 ---
--- Dies ist die Doku zu einer DemoFunktion
+-- Aktiviert oder deaktiviert die Single Stop Buttons. Single Stop ermöglicht
+-- das Anhalten eines einzelnen Betriebes, anstelle des Anhaltens aller
+-- Betriebe des gleichen Typs.
 --
--- Die DemoFunktion kann eine Ausführliche Beschreibung enthalten. Was alles möglich ist sollte in der Dokumentation von ldoc nachgeschlagen werden.
---
--- Bei der Angabe von Parametern kann jeweils angegeben werden welchen Type diese haben sollten, diese Information kann aber auch weggelassen werden.
---
--- @param[type=number] _Parameter1 Ein Paramter
--- @param[type=string] _Parameter2 (Optional) ein anderer Parameter
--- @return[type=table] Liste mit Ergebnissen
+-- @param[type=boolean] _Flag Single Stop nutzen
 -- @within Anwenderfunktionen
+-- @see API.AddBuildingButton
 --
 -- @usage
--- API.DemoFunktion(1, "_Parameter2")
+-- -- Single Stop nutzen
+-- API.UseSingleStop(true)
+-- -- Single Stop deaktivieren
+-- API.UseSingleStop(false)
 --
-function API.DemoFunktion(_Parameter1, _Parameter2)
-    
+function API.UseSingleStop(_Flag)
+    if _Flag == true then
+        Addon_SingleButtons.Local:AddSingleStopButton()
+    else
+        Addon_SingleButtons.Local:DropSingleStopButton()
+    end
+end
+
+---
+-- Aktiviert oder deaktiviert die Single Reserve Buttons. Single Reserve ermöglicht
+-- das Anhalten des Verbrauchs eines Gebäudetyps.
+--
+-- @param[type=boolean] _Flag Single Reserve nutzen
+-- @within Anwenderfunktionen
+-- @see API.AddBuildingButton
+--
+-- @usage
+-- -- Single Reserve nutzen
+-- API.UseSingleReserve(true)
+-- -- Single Reserve deaktivieren
+-- API.UseSingleReserve(false)
+--
+function API.UseSingleReserve(_Flag)
+    if _Flag == true then
+        Addon_SingleButtons.Local:AddSingleReserveButton()
+    else
+        Addon_SingleButtons.Local:DropSingleReserveButton()
+    end
+end
+
+---
+-- Aktiviere oder deaktiviere Rückbau bei Stadt- und Rohstoffgebäuden. Die
+-- Rückbaufunktion erlaubt es dem Spieler bei Stadt- und Rohstoffgebäude
+-- der Stufe 2 und 3 jeweils eine Stufe zu zerstören. Der überflüssige
+-- Arbeiter wird entlassen.
+--
+-- @param[type=boolean] _Flag Downgrade nutzen
+-- @within Anwenderfunktionen
+-- @see API.AddBuildingButton
+--
+-- @usage
+-- -- Downgrade nutzen
+-- API.UseDowngrade(true)
+-- -- Downgrade deaktivieren
+-- API.UseDowngrade(false)
+--
+function API.UseDowngrade(_Flag)
+    if _Flag == true then
+        Addon_SingleButtons.Local:AddSingleDowngradeButton()
+    else
+        Addon_SingleButtons.Local:DropSingleDowngradeButton()
+    end
 end
