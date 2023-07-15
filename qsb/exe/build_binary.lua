dc197305ad0c51d3628791f1b4f960208a4d6c18
Siedelwood <119606836+Siedelwood@users.noreply.github.com>
2023-07-15 15:13:20 +0200

    Update build_binary.lua
    assert um das io.open gesetzt zum testen der prereqs

--- qsb/exe/build_binary.lua
+++ qsb/exe/build_binary.lua
@@ -3,6 +3,6 @@ NameOfFileOrigin = arg[1]
 NameOfDestination = arg[2]
 
 Chunk = loadfile(NameOfFileOrigin .. ".lua")
-Output = io.open(NameOfDestination .. '.bin', 'wb')
+Output = assert(io.open(NameOfDestination .. '.bin', 'wb'))
 Output:write(string.dump(Chunk))
 Output:close()
