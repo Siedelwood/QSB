ab3f794d69f3cb6882a1dc4ebc8057f045f1be22
Siedelwood <119606836+Siedelwood@users.noreply.github.com>
2023-07-15 15:34:21 +0200

    Update build_binary.lua
    build_binary.lua debugging

--- qsb/exe/build_binary.lua
+++ qsb/exe/build_binary.lua
@@ -3,6 +3,10 @@ NameOfFileOrigin = arg[1]
 NameOfDestination = arg[2]
 
 Chunk = loadfile(NameOfFileOrigin .. ".lua")
+print(NameOfFileOrigin)
+print(NameOfDestination)
+print(Chunk)
 Output = assert(io.open(NameOfDestination .. '.bin', 'wb'))
+print(Output)
 Output:write(string.dump(Chunk))
 Output:close()
